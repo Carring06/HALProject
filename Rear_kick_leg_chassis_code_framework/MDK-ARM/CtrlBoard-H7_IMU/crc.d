90025a6fa77b066ee161031ea698f2de91c523a5
“Carring06” <3152067902@qq.comgit config --global user.email 3152067902@qq.com>
2026-04-26 13:06:48 +0800

    newRemote and AGV

--- Rear_kick_leg_chassis_code_framework/MDK-ARM/CtrlBoard-H7_IMU/crc.d
+++
@@ -1,5 +0,0 @@
-ctrlboard-h7_imu/crc.o: ..\User\Algorithm\CRC\CRC.c \
-  ..\User\Algorithm\CRC\CRC.h \
-  D:\Keil\ARM\ARMCLANG\bin\..\include\stdint.h \
-  D:\Keil\ARM\ARMCLANG\bin\..\include\stdbool.h \
-  D:\Keil\ARM\ARMCLANG\bin\..\include\string.h
